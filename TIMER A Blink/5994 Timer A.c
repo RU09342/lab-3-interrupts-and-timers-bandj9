d9582e703d20cd27287e34ab9a980e7a88fd4957
bandj9 <31831639+bandj9@users.noreply.github.com>
2017-10-09 20:42:08 -0400

    Add files via upload

---
+++ TIMER A Blink/5994 Timer A.c
@@ -0,0 +1,29 @@
+#include <msp430.h> 
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    //needed to reset device
+    P1SEL0 = 0x00; //sets P1 to GPIO
+    P1SEL1 = 0x00; //sets P1 to GPIO
+    P1DIR = BIT0; //sets LED at P1.0 to output
+
+    PM5CTL0 &= ~LOCKLPM5;
+
+    __bis_SR_register(GIE); //global interupt enable (enables all maskable interupts)
+
+    TA0CCTL0 = CCIE; // CCR0 interrupt enabled
+    TA0CCR0 = 60000; //register that timer will compare itself too
+    TA0CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK, upmode, clear TAR
+
+    for(;;){}
+    return 0;
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void){
+            P1OUT ^= BIT0; //toggles LED when interupt triggered
+}
